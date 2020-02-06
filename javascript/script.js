d5eba6e62402dfa5a1acf9b9da5d2babbe025892
Karina Aquino <karinaapthomaz@gmail.com>
2020-02-06 15:49:49 -0300

    Até aqui tudo certo

--- javascript/script.js
+++ javascript/script.js
@@ -1,13 +1,51 @@
 let btnAddTarefa = document.querySelector("#adicionar-tarefa");
-
 let tarefas = document.querySelector("#tarefas");
-
 let inputTarefa = document.querySelector("#tarefa-digitada");
+let listaTarefas = localStorage.getItem("listaTarefas") ? JSON.parse(localStorage.getItem("listaTarefas")) : [];
 
-btnAddTarefa.onclick = function () {
-    let valorDigitado = inputTarefa.value;
+const salvarLocalStorage = tarefas => {
+    let tarefasEmJson = JSON.stringify(tarefas)
+    localStorage.setItem("listaTarefas", tarefasEmJson);
+    console.log("Lista de tarefas salva com sucesso");
+}
 
-    let tarefaNova = ` <div class="col-md-4 [">
+const mostrarNaTela = (arrayTarefas) => {
+    arrayTarefas.forEach(textoTarefa => {
+        let tarefaNova = `<div class="col-md-4 ">
+        <div class="card-tarefa rounded">
+            <div class="texto-tarefa">
+                ${textoTarefa}
+            </div>
+            <div class="botao-tarefa">
+                <img src="images/check.png" alt="Botão para finalizar tarefa" title="Botão para finalizar tarefa" width="32" />
+            </div>
+    </div>
+    </div>`
+        tarefas.insertAdjacentHTML('beforeend', tarefaNova);
+
+        let objTarefaNova = tarefas.lastElementChild;
+        let btnCheckTarefaNova = objTarefaNova.lastElementChild.lastElementChild
+        btnCheckTarefaNova.onclick = (event) => {
+            event.target.parentNode.parentNode.parentNode.remove();
+        }
+    })
+}
+
+mostrarNaTela(listaTarefas);
+
+const criarTarefaComEnter = (event) => {
+
+    if (event.keyCode == 13 || event.type == "click") {
+        let valorDigitado = inputTarefa.value;
+
+        if (valorDigitado == "") {
+            alert("Digite a tarefa primeiro!");
+            return;
+        }
+        listaTarefas.push(valorDigitado);
+        salvarLocalStorage(listaTarefas);
+
+        let tarefaNova = `<div class="col-md-4 ">
     <div class="card-tarefa rounded">
         <div class="texto-tarefa">
             ${valorDigitado}
@@ -18,6 +56,16 @@ btnAddTarefa.onclick = function () {
 </div>
 </div>`
 
-    tarefas.innerHTML+=tarefaNova;
-    inputTarefa.value="";
+        tarefas.insertAdjacentHTML('beforeend', tarefaNova);
+        inputTarefa.value = "";
+
+        let objTarefaNova = tarefas.lastElementChild;
+        let btnCheckTarefaNova = objTarefaNova.lastElementChild.lastElementChild
+        btnCheckTarefaNova.onclick = (event) => {
+            event.target.parentNode.parentNode.parentNode.remove();
+        }
+    }
 }
+
+inputTarefa.addEventListener('keypress', criarTarefaComEnter)
+btnAddTarefa.addEventListener('click', criarTarefaComEnter)
